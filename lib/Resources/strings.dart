faf8d611591bb1d04b6660a131931127dbb90ba4
atefelsaid3 <atefelsaid3@gmail.com>
2023-03-20 10:22:57 +0200

    V_11

--- lib/Resources/strings.dart
+++ lib/Resources/strings.dart
@@ -1,5 +1,5 @@
 class AppStrings{
- static const appVersion='V1.0.1+10';
+ static const appVersion='V1.0.1+11';
  static const  hhsQ1='What best describes this dwelling type?';
  static const  hhsQ2='Is this dwelling...';
  static const  hhsQ3='How many bedrooms are there in the accommodation you live in?';
